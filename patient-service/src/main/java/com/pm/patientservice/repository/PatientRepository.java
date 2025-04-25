8b9653b61c523684f2c858ed321ff43307e708fc
NOMAN AHMED <nomiimam@gmail.com>
2025-04-25 22:08:49 +0500

    updated

--- patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+    boolean existsByEmail(String email);
 }
--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -2,12 +2,12 @@ package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,8 +25,16 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+
+        if(existsByEmail(patientRequestDTO.getEmail())){
+           throw new EmailAlreadyExistsException("A Patient email already exist " + patientRequestDTO.getEmail());
+        }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
+    }
+
+    public boolean existsByEmail(String email) {
+        return patientRepository.existsByEmail(email);
     }
 
 }
